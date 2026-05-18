d91752daa9c28f3a41858027b938b0da40621da0
Ricky Cooper <ethanrickyjrjr@gmail.com>
2026-05-18 01:29:15 -0400

    feat(bls-qcew): scaffold package skeleton + FIPS constants

---
+++ ingest/pipelines/bls_qcew/constants.py
@@ -0,0 +1,8 @@
+BLS_QCEW_BASE_URL = "https://data.bls.gov/cew/data/api"
+
+# Area FIPS codes for the three geographies we track
+AREA_FIPS = {
+    "florida":  "12000",
+    "lee":      "12071",
+    "collier":  "12021",
+}
